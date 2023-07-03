a6566cc3ae2cd736e0b11e088c30e7441fadd65e
narendrasingh26 <narendrajadon261990@gmail.com>
2023-07-03 14:47:41 +0530

    task3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 // import Button from 'react-bootstrap/Button';
 import Login from './component/pages/Login'
+import Main from './component/pages/Main'
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Login/>
-       
+       <Route path='/main'>
+        <Main/>
+       </Route>
     </div>
   )
 }
---
+++ src/component/pages/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <h1>WELCOME TO MAIL-BOX-CLIENT-PROJECT</h1>
+    </div>
+  )
+}
+
+export default Main
